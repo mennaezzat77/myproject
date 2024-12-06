765f7b7bbc9cce9484e22eb2ec377e60e912215c
menna <mennaezzat130905@gmail.com>
2024-12-06 10:04:29 +0200

    adding main

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Admin ad1 = new Admin(1,"gmail", "moh");
+        Admin ad1 = new Admin(1,"yg", "agust d");
 
         ad1.check();
     }
